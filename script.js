c2914f43106d3b34f21d694345c18b2dc86b4e5e
Andrew Hart <829024+arhart@users.noreply.github.com>
2021-09-06 16:21:54 -0600

    demo isSecureContext

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const p = document.createElement('p');
 const t = document.createTextNode( 'dynamically added text inside a dynamically created paragraph' );
 p.appendChild( t );
 b.appendChild( p );
+const table = document.createElement('table');
+const headRow = document.createElement('tr');
+const $headerName = document.createElement('th');
+$headerName.appendChild( document.createTextNode( 'Name' ) );
+headRow.appendChild( $headerName );
+const $headerValue = document.createElement('th');
+$headerValue.appendChild( document.createTextNode( 'Value' ) );
+headRow.appendChild( $headerValue );
+table.appendChild( headRow );
+for(const i of ['isSecureContext', 'crypto']) {
+  const tr = document.createElement('tr');
+  const $name = document.createElement('td');
+  $name.appendChild( document.createTextNode( i ) );
+  const $value = document.createElement('td');
+  $value.appendChild( document.createTextNode( window[i] ) );
+  tr.appendChild( $name );
+  tr.appendChild( $value );
+  table.appendChild( tr );
+}
+b.appendChild( table );
